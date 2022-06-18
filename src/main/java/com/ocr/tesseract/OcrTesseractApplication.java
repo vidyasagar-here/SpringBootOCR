5fb5a859b7b5c1e85e95b8ed8ce69edd3bddb831
unknown <vidyasagar.here@gmail.com>
2022-06-18 10:35:51 +0530

    OCR changes

---
+++ src/main/java/com/ocr/tesseract/OcrTesseractApplication.java
@@ -0,0 +1,20 @@
+package com.ocr.tesseract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+/**
+ * @author Sagar
+ *
+ */
+@SpringBootApplication
+@ComponentScan({"com.ocr.*"})
+public class OcrTesseractApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcrTesseractApplication.class, args);
+	}
+
+}
